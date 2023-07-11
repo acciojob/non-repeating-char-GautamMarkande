c11830e9654a3f8a18e8dd3ddef08ae299fd8eaf
GautamMarkande <118103083+GautamMarkande@users.noreply.github.com>
2023-07-11 22:36:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+let str = prompt("Enter a string");
+
+str.trim();
+
+let map = new Map();
+for(let i = 0; i<str.length; i++){
+    if(map.has(str[i])){
+        map.set(str[i],map.get(str[i])+1);
+    }else{
+        map.set(str[i],1);
+    }
+}
+//console.log(map);
+for(let i = 0; i<str.length; i++){
+    if(map.get(str[i])===1){
+        alert(str[i]);
+       // console.log(str[i]);
+        break;
+    }
+}
